e9dd871f8845b28a98a36d2cb0609f9db1fd777c
frontjss <frontjss@gmail.com>
2017-12-02 14:29:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+import telebot
+# import cherrypy
+import requests, json
+from settings import *
+
+token = "380396463:AAHy5oNoYG8-h9Rc465i9fVp1f81VWdxJXU"
+
+# WEBHOOK_HOST = '194.67.217.180'
+# WEBHOOK_PORT = 8443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+# WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+
+# WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
+# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
+
+# WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+# WEBHOOK_URL_PATH = "/%s/" % token
+
+bot = telebot.TeleBot(token)
+
+# # Наш вебхук-сервер
+# class WebhookServer(object):
+#     @cherrypy.expose
+#     def index(self):
+#         if 'content-length' in cherrypy.request.headers and \
+#                         'content-type' in cherrypy.request.headers and \
+#                         cherrypy.request.headers['content-type'] == 'application/json':
+#             length = int(cherrypy.request.headers['content-length'])
+#             json_string = cherrypy.request.body.read(length).decode("utf-8")
+#             update = telebot.types.Update.de_json(json_string)
+#             # Эта функция обеспечивает проверку входящего сообщения
+#             bot.process_new_updates([update])
+#             return ''
+#         else:
+#             raise cherrypy.HTTPError(403)
+
+class Method:
+    def __init__(self, method):
+        self.request = {"method": "", "param": {}}
+        self.request.update({"method": method})
+
+    def transfer(self):
+        response = requests.post('http://194.67.217.180:8484/bot/param', json=self.request)
+        response = json.loads(response.content.decode("utf-8"))
+        return response
+
+    def param(self, **kwargs):
+        self.request["param"] = kwargs
+        return True
+
+def generator_menu(menu_list, dop=None):
+    user_markup = telebot.types.ReplyKeyboardMarkup()
+    for item in menu_list:
+        user_markup.row(item)
+    if dop is not None:
+        user_markup.row(dop)
+    return user_markup
+
+def getMessage():
+    bot.send_message(message.chat.id, "Hi")
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    a = Method("addUser")
+    AddUser = {
+        "telegram": message.from_user.id,
+        "username": message.chat.first_name
+    }
+    print(AddUser)
+    a.param(**AddUser)
+    result = a.transfer()
+    print(result)
+    bot.send_message(message.chat.id, text_welcome, reply_markup=generator_menu(menu))
+
+@bot.message_handler(regexp='Ближайшие водоматы')
+def handle_start(message):
+    bot.send_message(message.chat.id, wada, reply_markup=generator_menu(back_menu_list))
+    bot.send_message(message.chat.id, f, reply_markup=generator_menu(back_menu_list))
+
+@bot.message_handler(regexp='Баланс')
+def handle_start(message):
+    a = Method("score")
+    Score = {
+        "telegram": message.from_user.id
+    }
+    a.param(**Score)
+    result = a.transfer()
+    print(result)
+    result = result["score"] / 400
+    bot.send_message(message.chat.id, str(result) + " литров", reply_markup=generator_menu(menu))
+
+
+@bot.message_handler(regexp='Оставить отзыв')
+def handle_start(message):
+    sent = bot.send_message(message.chat.id, text_review, reply_markup=generator_menu(back_menu_list))
+    bot.register_next_step_handler(sent, feedback)
+
+def feedback(message):
+    if message.text == "Назад":
+        bot.send_message(message.chat.id, text_welcome, reply_markup=generator_menu(menu))
+    else:
+        a = Method("recall")
+        recall = {
+            "telegram": message.from_user.id,
+            "review": message.text
+        }
+        a.param(**recall)
+        result = a.transfer()
+        print(result)
+        bot.send_message(message.chat.id, text_review_answer, reply_markup=generator_menu(menu))
+
+
+@bot.message_handler(regexp='Подключиться к водомату')
+def handle_start(message):
+    sent = bot.send_message(message.chat.id, text_id, reply_markup=generator_menu(back_menu_list))
+    bot.register_next_step_handler(sent, startWM)
+
+def response(param):
+    return param["param"]
+
+
+def startWM(message):
+    if message.sticker:
+        bot.send_message(message.chat.id, command_error, reply_markup=generator_menu(menu))
+    elif message.text.isdigit():
+        a = Method("start")
+        Start = {
+            "telegram": message.from_user.id,
+            "wm": int(message.text)
+        }
+        a.param(**Start)
+        result = a.transfer()
+        print(result)
+        if result["situation"]:
+            bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(stop_menu_list))
+        else:
+            bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(menu))
+    elif message.text != "Назад":
+        bot.send_message(message.chat.id, command_error, reply_markup=generator_menu(menu))
+    else:
+        bot.send_message(message.chat.id, text_welcome, reply_markup=generator_menu(menu))
+
+
+@bot.message_handler(regexp='Остановить')
+def handle_start(message):
+    a = Method("stop")
+    Stop = {
+        "telegram": message.from_user.id
+    }
+    a.param(**Stop)
+    result = a.transfer()
+    print(result)
+    bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(menu))
+
+
+@bot.message_handler(regexp='Личный кабинет')
+def handle_start(message):
+    sent = bot.send_message(message.chat.id, text_get, reply_markup=generator_menu(menu[message.text]))
+
+
+@bot.message_handler(regexp='Назад')
+def handle_start(message):
+    sent = bot.send_message(message.chat.id, text_get, reply_markup=generator_menu(menu))
+
+
+# # Снимаем вебхук перед повторной установкой (избавляет от некоторых проблем)
+# bot.remove_webhook()
+
+# # Ставим заново вебхук
+# bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+#                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# # Указываем настройки сервера CherryPy
+# cherrypy.config.update({
+#     'server.socket_host': WEBHOOK_LISTEN,
+#     'server.socket_port': WEBHOOK_PORT,
+#     'server.ssl_module': 'builtin',
+#     'server.ssl_certificate': WEBHOOK_SSL_CERT,
+#     'server.ssl_private_key': WEBHOOK_SSL_PRIV
+# })
+
+# # Собственно, запуск!
+# cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
+bot.polling(none_stop=True, interval = 0)
