8502852d5a28826936771e631fe18a6e4695c71f
frontjss <frontjss@gmail.com>
2017-12-15 18:59:51 +0300

    new

--- server.py
+++ server.py
@@ -9,9 +9,9 @@ def server():
     method = request.json.get('method')
     param = request.json.get('param')
     if method == 'stop':
-    	response = stop(param)
+		response = stop(param)
 	if method == 'dispatch':
-		response = dispatch(param)
+    	response = dispatch(param)
 	if method == 'upscore':
 		response = upscore(param)
 	return json.dumps(response)
