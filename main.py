a57f07129a471d36489538c55aabc52467eba948
frontjss <frontjss@gmail.com>
2017-12-15 15:22:58 +0300

    new

--- main.py
+++ main.py
@@ -19,21 +19,21 @@ WEBHOOK_URL_PATH = "/%s/" % token
 bot = telebot.TeleBot(token)
 
 
-# # Наш вебхук-сервер
-# class WebhookServer(object):
-#     @cherrypy.expose
-#     def index(self):
-#         if 'content-length' in cherrypy.request.headers and \
-#                         'content-type' in cherrypy.request.headers and \
-#                         cherrypy.request.headers['content-type'] == 'application/json':
-#             length = int(cherrypy.request.headers['content-length'])
-#             json_string = cherrypy.request.body.read(length).decode("utf-8")
-#             update = telebot.types.Update.de_json(json_string)
-#             # Эта функция обеспечивает проверку входящего сообщения
-#             bot.process_new_updates([update])
-#             return ''
-#         else:
-#             raise cherrypy.HTTPError(403)
+# Наш вебхук-сервер
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            # Эта функция обеспечивает проверку входящего сообщения
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
 
 class Method:
     def __init__(self, method):
