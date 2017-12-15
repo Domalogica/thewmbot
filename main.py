6a39163fb271a30c15e0d3679b20cb348ec920b9
frontjss <frontjss@gmail.com>
2017-12-15 17:43:24 +0300

    new

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from settings import *
 from flask import Flask
 
 
-app = Flask(__name__)
+app.run(host='194.67.217.180', port=8383)
 
 @app.route('/server/param', methods=['GET', 'POST'])
 def server():
@@ -256,8 +256,6 @@ cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
 
 
 
-
-app.run(host='194.67.217.180', port=8383)
 
 
 
