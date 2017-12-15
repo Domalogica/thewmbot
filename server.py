24fbb87a6d2130f3227378a97bb9e26e16eff5f8
frontjss <frontjss@gmail.com>
2017-12-15 18:04:28 +0300

    new

--- server.py
+++ server.py
@@ -7,8 +7,11 @@ app = Flask(__name__)
 def server():
     if request.method == 'POST':
         print("POST")
+        return json.dumps("POST")
+
     else:
         print("GET")
+        return json.dumps("GET")
 
 def run():
     app.run(host='194.67.217.180', port=8383)
