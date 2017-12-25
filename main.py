25e98fd71fc5d01084a7bdcae14b531bf07e837a
frontjss <frontjss@gmail.com>
2017-12-25 11:27:43 +0300

    new

--- main.py
+++ main.py
@@ -180,11 +180,8 @@ def startWM(message):
         else:
             print(result)
             bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(main_menu_list))
-            return True
     elif message.text == "Назад":
         bot.send_message(message.chat.id, text_welcome, reply_markup=generator_menu(main_menu_list))
-    else:
-        bot.send_message(message.chat.id, command_error, reply_markup=generator_menu(main_menu_list))
 
 
 
