838513ad0f32c7c4d3080d821f6e8d2a57062894
frontjss <frontjss@gmail.com>
2017-12-27 16:59:57 +0300

    new

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ def startWM(message):
             a.param(**Score)
             result1 = a.transfer()
             result1 = str(result1["score"] / 400)
-            print(call.message.from_user.id)
+            print(message.from_user.id)
             send = bot.send_message(message.chat.id, response(result) + text_water + str(result1)[:5:] + " литров", reply_markup=generator_menu(stop_menu_list))
             # chatID = message.chat.id
             # server.message_id.update({chatID: {'message_id': send.message_id}})
