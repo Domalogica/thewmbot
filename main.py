c38ff998372ea354053f2e3cf23125fca0b8ef9e
frontjss <frontjss@gmail.com>
2017-12-27 16:37:40 +0300

    new

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def callback_data(call):
         result1 = a.transfer()
         result1 = str(result1["score"] / 400)
         bot.send_message(message.chat.id, response(result) + text_water, reply_markup=generator_menu(stop_menu_list))
-        send = bot.send_message(message.chat.id, "Ваш баланс: " + str(result1)[:5:] + " литров", reply_markup=generator_menu(stop_menu_list))
+        send = bot.send_message(message.chat.id, "\nВаш баланс: " + str(result1)[:5:] + " литров", reply_markup=generator_menu(stop_menu_list))
     else:
         bot.send_message(call.message.chat.id, response(result), reply_markup=generator_menu(main_menu_list))
     chatID = call.message.chat.id
@@ -190,7 +190,7 @@ def startWM(message):
             result1 = a.transfer()
             result1 = str(result1["score"] / 400)
             bot.send_message(message.chat.id, response(result) + text_water, reply_markup=generator_menu(stop_menu_list))
-            send = bot.send_message(message.chat.id, "Ваш баланс: " + str(result1)[:5:] + " литров", reply_markup=generator_menu(stop_menu_list))
+            send = bot.send_message(message.chat.id, "\nВаш баланс: " + str(result1)[:5:] + " литров", reply_markup=generator_menu(stop_menu_list))
             chatID = message.chat.id
             server.message_id.update({chatID: {'message_id': send.message_id}})
         else:
