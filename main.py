6de679d71a2968e48882f9a6c299b731d83f7a5f
frontjss <frontjss@gmail.com>
2018-01-17 16:54:27 +0300

    new

--- main.py
+++ main.py
@@ -57,7 +57,6 @@ class MethodGet:
             print(self.request)
             response = requests.get('http://194.67.217.180:8484/get_state', json=self.request)
             logging.debug(response.text)
-            response = json.loads(response.content.decode("utf-8"))
         except ConnectionError as e:
             logging.error(self.request)
             logging.error(u'ConnectionError')
