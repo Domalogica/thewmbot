680cf6e86a70f1b6333e79b82affeb6bdfc5166c
frontjss <frontjss@gmail.com>
2018-01-23 18:59:08 +0300

    new

--- main.py
+++ main.py
@@ -258,20 +258,20 @@ def handle_start(message):
     bot.send_message(message.chat.id, text_get, reply_markup=generator_menu(main_menu_list))
 
 
-@bot.message_handler(regexp='^Статистика$')
-def handle_start(message):
-    logging.info(message.text)
-    a = MethodGet("statistic")
-    result = a.transfer()
-    bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(back_menu_list))
+# @bot.message_handler(regexp='^Статистика$')
+# def handle_start(message):
+#     logging.info(message.text)
+#     a = MethodGet("statistic")
+#     result = a.transfer()
+#     bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(back_menu_list))
 
 
-@bot.message_handler(regexp='Активные водоматы')
-def handle_start(message):
-    logging.info(message.text)
-    a = MethodGet("connected_vodomats")
-    result = a.transfer()
-    bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(back_menu_list))
+# @bot.message_handler(regexp='Активные водоматы')
+# def handle_start(message):
+#     logging.info(message.text)
+#     a = MethodGet("connected_vodomats")
+#     result = a.transfer()
+#     bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(back_menu_list))
 
 
 
