1613ccaaf3e09f47b8fc583d5ad7eb6cd31acae6
frontjss <frontjss@gmail.com>
2018-02-03 18:27:39 +0300

    new

--- main.py
+++ main.py
@@ -386,7 +386,9 @@ def handle_start(message):
 @bot.message_handler(regexp='^Статистика$')
 def handle_start(message):
     logging.info(message.text)
-    bot.send_message(message.chat.id, text_get, reply_markup=generator_menu(my_stat + back_menu_list))
+    a = MethodGet("statistic")
+    result = a.transfer()
+    bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(back_menu_list))
 
 
 @bot.message_handler(regexp='Текущее состояние')
