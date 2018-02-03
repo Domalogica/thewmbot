83cc1414ed7b34d3a9645b8a747e44727c434ea4
frontjss <frontjss@gmail.com>
2018-02-03 18:30:41 +0300

    new

--- main.py
+++ main.py
@@ -394,8 +394,7 @@ def handle_start(message):
     logging.info(message.text)
     a = MethodGet("statistic")
     result = a.transfer()
-    
-    print(result)
+    print(response(result))
     bot.send_message(message.chat.id, response(result), reply_markup=generator_menu(back_menu_list))
 
 
