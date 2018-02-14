3b4d5c9b7d53d74fc1ca0cba17644887f76121bc
frontjss <frontjss@gmail.com>
2018-02-14 12:07:44 +0300

    new

--- main.py
+++ main.py
@@ -311,17 +311,26 @@ def handle_start(message):
         sheet1.write(0, 1, "Продажи")
         sheet1.write(0, 2, "Наличка в водомате")
         sheet1.write(0, 3, "Дата/время")
+        a = "0"
+        b = "0"
+        c = "0"
+        d = "0"
         j = 1
         for x in response:
-            i = 0;
-            sheet1.write(j, i, str(x["wm"]))
-            i+=1
-            sheet1.write(j, i, str(x["totalPaid"]))
-            i+=1
-            sheet1.write(j, i, str(x["totalHardCash"]))
-            i+=1
-            sheet1.write(j, i, str(x["updated"]))
-            j+=1
+            if b != x["totalPaid"] and c != x["totalHardCash"]:
+                i = 0;
+                sheet1.write(j, i, str(x["wm"]))
+                i+=1
+                sheet1.write(j, i, str(x["totalPaid"]))
+                i+=1
+                sheet1.write(j, i, str(x["totalHardCash"]))
+                i+=1
+                sheet1.write(j, i, str(x["updated"]))
+                j+=1
+            a = x["wm"]
+            b = x["totalPaid"]
+            c = x["totalHardCash"]
+            d = x["updated"]
     except Exception as e:
         print(e)
 
