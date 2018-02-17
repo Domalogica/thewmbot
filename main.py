267b081c4f31c0747487482ed4b567adc143f965
frontjss <frontjss@gmail.com>
2018-02-17 16:01:28 +0300

    new

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ def handle_start(message):
                     }
                     wmsession.update({session["wm"]: properties})
         except Exception as e:
-            sheet = book.add_sheet(wm)
+            sheet = book.add_sheet(str(wm))
             sheet.write(0, 0, "Продажи")
             sheet.write(0, 1, "Наличка в водомате")
             sheet.write(0, 2, "Дата/время")
