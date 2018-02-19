c048deee39fe9619f9534e95311ed25b91f456ad
frontjss <frontjss@gmail.com>
2018-02-19 17:12:17 +0300

    new

--- main.py
+++ main.py
@@ -298,7 +298,7 @@ def handle_start(message):
 
     response = requests.get('http://194.67.217.180:8484/get_state', json=data)
     response = json.loads(response.content.decode("utf-8"))
-    
+
     book = xlwt.Workbook(encoding="utf-8")
     wmsession = {}
     sheet = []
@@ -320,7 +320,6 @@ def handle_start(message):
                     # wmsession[wm]["sheet"].write(index, 0, str(wmsession[wm]["totalPaid"]))
                     # wmsession[wm]["sheet"].write(index, 1, str(wmsession[wm]["totalHardCash"]))
                     # wmsession[wm]["sheet"].write(index, 2, str(wmsession[wm]["updated"]))
-                    print(wmsession)
 
         except KeyError as e:
             ID = "ID " + str(wm)
@@ -333,7 +332,7 @@ def handle_start(message):
             }
             wmsession.update({wm: properties})
 
-
+            print(wmsession)
             wmsession[wm]["sheet"].write(0, 0, "Продажи")
             wmsession[wm]["sheet"].write(0, 1, "Наличка в водомате")
             wmsession[wm]["sheet"].write(0, 2, "Дата/время")
