ec174b15169383db81d4b23b4cfbfe7f979e379f
frontjss <frontjss@gmail.com>
2018-02-19 16:02:27 +0300

    new

--- main.py
+++ main.py
@@ -316,11 +316,10 @@ def handle_start(message):
                         "updated": str(session["updated"])
                     }
                     wmsession.update({wm: properties})
-                    
+                    print(wmsession[wm])
                     wmsession[wm]["sheet"].write(index, 0, str(wmsession[wm]["totalPaid"]))
                     wmsession[wm]["sheet"].write(index, 1, str(wmsession[wm]["totalHardCash"]))
                     wmsession[wm]["sheet"].write(index, 2, str(wmsession[wm]["updated"]))
-                    print(wmsession[wm])
         except KeyError as e:
             ID = "ID " + str(wm)
 
